dca6da9a8b4e095a143420ff7737c332e5b61e1b
M SaLeH NYM <84126999+MSaLeHNYM@users.noreply.github.com>
2023-08-14 11:27:04 +0330

    Create showallusers.sh

---
+++ core/showallusers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+csv_file="../database/users.csv"
+
+GREEN="\e[32m"
+YELLOW="\e[33m"
+CYAN="\e[36m"
+RESET="\e[0m"
+
+echo -e "${CYAN}All Users with Passwords and Connection Limits:${RESET}"
+echo "----------------------------------------------"
+
+while IFS=',' read -r username userpass userlimit; do
+    echo -e "${GREEN}Username:${RESET} $username"
+    echo -e "${YELLOW}Password:${RESET} $userpass"
+    echo -e "${GREEN}Limit:${RESET} $userlimit"
+    echo "----------------------------------------------"
+done < "$csv_file"
