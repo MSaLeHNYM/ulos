b08fefe2a014a085d896d34e58a1c7702280e3a0
M SaLeH NYM <84126999+MSaLeHNYM@users.noreply.github.com>
2023-08-14 10:56:15 +0330

    Create deleteuser.sh

---
+++ deleteuser.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+csv_file="users.csv"
+pam_limits_file="/etc/security/limits.conf"
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <username>"
+    exit 1
+fi
+
+username="$1"
+
+# Check if the user exists in the CSV file
+if grep -q "^$username," "$csv_file"; then
+    # Remove the user entry from the CSV file
+    grep -v "^$username," "$csv_file" > temp.csv
+    mv temp.csv "$csv_file"
+    
+    echo "User '$username' deleted from users.csv."
+
+    # Remove PAM limits for the user
+    if sudo sed -i "/^$username\s*hard\s*nproc\s*/d" "$pam_limits_file"; then
+        echo "PAM limits removed for user '$username'."
+    else
+        echo "PAM limits for user '$username' not found in $pam_limits_file."
+    fi
+else
+    echo "User '$username' not found in users.csv."
+fi
