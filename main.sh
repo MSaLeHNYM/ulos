1808a7bc59f22364cf5ac48b8b7566814c18344a
M SaLeH NYM <84126999+MSaLeHNYM@users.noreply.github.com>
2023-08-14 10:41:47 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,32 +1,53 @@
 #!/bin/bash
 
+chmod +x showonlineusers.sh
+chmod +x updateusers.sh
+chmod +x adduser.sh
+chmod +x addlimit.sh
 
-chmod +x ./updateusers.sh
-chmod +x ./addlimit.sh
+GREEN="\e[32m"
+YELLOW="\e[33m"
+RESET="\e[0m"
 
+echo "Welcome to the ${YELLOW}User Management Script${RESET}"
+echo "Select an option:"
+echo "1. Show Online Users"
+echo "2. Update Users"
+echo "3. Add User"
+echo "4. Add User Limits"
+echo "5. Exit"
 
-print_menu() {
-    echo "Select an option:"
-    echo "1. Update Users"
-    echo "2. Add Connection Limits"
-    echo "3. Quit"
-}
+read -p "Enter your choice: " choice
 
-read_option() {
-    read -p "Enter your choice: " choice
-    case $choice in
-        1) ./updateusers.sh ;;
-        2) 
-            read -p "Enter the username: " username
-            read -p "Enter the connection limit: " limit
-            ./addlimit.sh "$username" "$limit"
-            ;;
-        3) echo "Exiting." ; exit 0 ;;
-        *) echo "Invalid option. Please select a valid option." ;;
-    esac
-}
-
-while true; do
-    print_menu
-    read_option
-done
+case "$choice" in
+    1)
+        echo -e "${GREEN}Fetching online users...${RESET}"
+        sleep 1
+        ./showonlineusers.sh
+        ;;
+    2)
+        echo -e "${GREEN}Updating users...${RESET}"
+        sleep 1
+        ./updateusers.sh
+        ;;
+    3)
+        echo -e "${GREEN}Adding a new user...${RESET}"
+        sleep 1
+        ./adduser.sh
+        ;;
+    4)
+        read -p "Enter username: " username
+        read -p "Enter connection limit: " connection_limit
+        echo -e "${GREEN}Adding limits for user...${RESET}"
+        sleep 1
+        ./addlimit.sh "$username" "$connection_limit"
+        ;;
+    5)
+        echo -e "${GREEN}Exiting...${RESET}"
+        sleep 1
+        exit
+        ;;
+    *)
+        echo -e "${YELLOW}Invalid choice${RESET}"
+        ;;
+esac
